1cee300a619e4ced1fc6729b728b9516d45d0596
Felix Harrison <fmlharrison@gmail.com>
2016-11-16 11:17:32 +0000

    set up relationship between tables

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
+  belongs_to :restaurant
+
   validates_presence_of :rating
   validates_inclusion_of :rating, :in => 1..5
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115155337) do
+ActiveRecord::Schema.define(version: 20161116111116) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(version: 20161115155337) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
   end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
 
 end
